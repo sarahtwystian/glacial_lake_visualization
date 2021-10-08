f805185d99b1fb435e6105cadb9c1314dc2e6409
sarahtwystian <66697346+sarahtwystian@users.noreply.github.com>
2021-10-08 16:22:22 -0500

    Hierarchical selections

--- visualization.R
+++ visualization.R
@@ -8,29 +8,115 @@ library(purrr)
 library(stringr)
 library(DT)
 library(tidyr)
+library(shinyjs)
 
 lakes <- read_csv("lake_areas.csv")
 
-
-
 ui <- fluidPage(
   sidebarPanel(
+    useShinyjs(),
     sliderInput("area", "Area:", min = 0, max = max(lakes$area), value = c(0,max(lakes$area)), step=0.001),
-    uiOutput("lakeSelector"),
-    uiOutput("basinSelector"),
-    #uiOutput("trendSelector"),
+    tabsetPanel(id = "tabset",
+                tabPanel("Sub basin",
+                         
+                         selectizeInput(
+                           inputId = "sub_basin",
+                           label = "1. Sub Basin",
+                           choices = c("Please choose some sub basins" = "", lakes$Sub_Basin),
+                           multiple = TRUE),
+                         #actionButton("reset_id1", "Reset"),
+                         selectizeInput(inputId = "lakeID_1",
+                                        label = "2. Lake ID",
+                                        choices = c("Please choose some lake IDs" = "",lakes$GL_ID),
+                                        multiple = TRUE),
+                         actionButton("reset_sb", "Reset")
+                         #uiOutput("lakeID_1")
+                         
+                ),
+                tabPanel("Lake ID",
+                         
+                         selectizeInput(inputId = "lakeID_2",
+                                        label = NULL,
+                                        choices = c("Please choose some lake IDs" = "",lakes$GL_ID),
+                                        multiple = TRUE),
+                         actionButton("reset_id2", "Reset")
+                         
+                         
+                )
+                #actionButton("go", "Plot")
+                
+    ),
     width=12
   ),
   
-  plotOutput("plot", brush = "plot_brush", height = 500, dblclick = "plot_reset"),
-  DT::dataTableOutput('table')
+  mainPanel(  plotOutput("plot", brush = "plot_brush", height = 500, dblclick = "plot_reset"),
+              DT::dataTableOutput('table'),
+              width=12)
+  
+  
 )
-server <- function(input, output, session) {
 
+server <- function(input, output){
+  id <- reactive({
+    ########update selections
+    if(length(input$sub_basin)!=0){
+      filter(lakes, Sub_Basin %in% input$sub_basin)
+    }
+  })
+  observeEvent(id(), {
+    choices <- unique(id()$GL_ID)
+    updateSelectizeInput(inputId = "lakeID_1", choices = c("Please choose some lake IDs" = "",choices))
+  })
+  
+  
+  
+  
+  #####clear selections
+  observeEvent(input$reset_sb, {
+    shinyjs::reset("sub_basin")
+    shinyjs::reset("lakeID_1")
+    shinyjs::reset("plot")
+  })
+  
+  observeEvent(input$reset_id2, {
+    shinyjs::reset("lakeID_2")
+    shinyjs::reset("plot")
+    
+  })
+  
+  
+  
+  ######
+
+  selected <- reactive({
+    if (input$tabset == "Sub basin"){
+      if(length(input$sub_basin)!=0&length(input$lakeID_1)==0){
+        df<-filter(lakes,Sub_Basin%in%input$sub_basin)
+      }else if(length(input$sub_basin)!=0&length(input$lakeID_1)!=0){
+        df<-filter(lakes, (GL_ID %in% input$lakeID_1) & (Sub_Basin %in% input$sub_basin))
+      }else{
+        df<-lakes
+      }
+      brushedPoints(df, input$plot_brush, "time", "area", allRows = TRUE) %>%
+        select(GL_ID, time, area, Sub_Basin, selected_) %>%
+        filter(selected_==TRUE)
+      
+    }else{
+      if(length(input$lakeID_2)!=0){
+        df<-filter(lakes, GL_ID %in% input$lakeID_2)
+      }else{
+        df<-lakes
+      }
+      brushedPoints(df, input$plot_brush, "time", "area", allRows = TRUE) %>%
+        select(GL_ID, time, area, Sub_Basin, selected_) %>%
+        filter(selected_==TRUE)
+    }
+    
+  })
+  
   output$plot <- renderPlot({
     y <- selected() %>%
       top_n(1e3)
-    print(y)
     
     if (nrow(y) == 0) {
       lakes$brush<-rep(FALSE, nrow(lakes))
@@ -42,15 +128,10 @@ server <- function(input, output, session) {
         mutate(brush = ifelse(is.na(selected_), sel, selected_)) %>% 
         select(GL_ID, time, area, Sub_Basin, brush)
     }
-    #print(y)
-    #print(head(lakes))
-    print("subbasin")
-    print(length(input$sub_basin)==0)
-    print("lakeid")
-    print(length(input$lakeID)==0)
     
-    if (length(input$sub_basin)!=0 & length(input$lakeID)==0){
-      print("1")
+    
+    if (length(input$sub_basin)!=0 & length(input$lakeID_1)==0){
+      
       ggplot(data=filter(lakes, Sub_Basin %in% input$sub_basin), aes(time, area)) +
         geom_point(aes(color=brush), size = 1) +
         geom_line(aes(group = GL_ID), size = 0.7, alpha = 0.5) +
@@ -59,9 +140,8 @@ server <- function(input, output, session) {
         xlab("Year")+
         scale_color_manual(values=c("black","red"))
     }
-    else if(length(input$lakeID)!=0 & length(input$sub_basin)==0){
-      print("2")
-      ggplot(data=filter(lakes, GL_ID %in% input$lakeID), aes(time, area)) +
+    else if(length(input$sub_basin)!=0 & length(input$lakeID_1)!=0){
+      ggplot(data=filter(lakes, (GL_ID %in% input$lakeID_1) & (Sub_Basin %in% input$sub_basin)), aes(time, area)) +
         geom_point(aes(color=brush), size = 1) +
         geom_line(aes(group = GL_ID), size = 0.7, alpha = 0.5) +
         scale_y_log10()+
@@ -69,9 +149,11 @@ server <- function(input, output, session) {
         xlab("Year")+
         scale_color_manual(values=c("black","red"))
     }
-    else if(length(input$sub_basin)!=0 & length(input$lakeID)!=0){
-      print("3")
-      ggplot(data=filter(lakes, (GL_ID %in% input$lakeID) & (Sub_Basin %in% input$sub_basin)), aes(time, area)) +
+    
+    
+    else if(length(input$lakeID_2)!=0){
+      
+      ggplot(data=filter(lakes, GL_ID %in% input$lakeID_2), aes(time, area)) +
         geom_point(aes(color=brush), size = 1) +
         geom_line(aes(group = GL_ID), size = 0.7, alpha = 0.5) +
         scale_y_log10()+
@@ -79,8 +161,8 @@ server <- function(input, output, session) {
         xlab("Year")+
         scale_color_manual(values=c("black","red"))
     }
+    
     else{
-      print("4")
       ggplot(data=lakes, aes(time, area))+
         geom_point(aes(color=brush),  size = 1, show.legend = FALSE) +
         geom_line(aes(group = GL_ID), size = 0.7, alpha = 0.5) +
@@ -88,38 +170,14 @@ server <- function(input, output, session) {
         ylim(input$area[1],input$area[2]) +
         xlab("Year")+
         scale_color_manual(values=c("black","red"))
-    } 
-    
-  },
-  res = 96, height = 250)
-  
-
-  
-  selected <- reactive({
-    #df<-filter(lakes,(Sub_Basin %in% input$sub_basin)&(GL_ID %in% input$lakeID))
-    df<-if(length(input$sub_basin)!=0){filter(lakes, Sub_Basin %in% input$sub_basin) }else{lakes}
-    df<-if(length(input$lakeID)!=0){filter(df, GL_ID %in% input$lakeID)}else{lakes}
-    if(length(input$sub_basin)!=0|length(input$lakeID)!=0){
-      brushedPoints(df, input$plot_brush, "time", "area", allRows = TRUE) %>%
-        select(GL_ID, time, area, Sub_Basin, selected_) %>% 
-        filter(selected_==TRUE)
-    }
-    else{
-      brushedPoints(lakes, input$plot_brush, "time", "area", allRows = TRUE) %>%
-        select(GL_ID, time, area, Sub_Basin, selected_) %>% 
-        filter(selected_==TRUE)
     }
   })
   
-  observeEvent(input$plot_reset, {
-    selected(rep(FALSE, nrow(lakes)))
-  })
   
   output$table = DT::renderDataTable({
     x <- selected() %>%
       select(GL_ID,time) %>% 
       top_n(1e3)
-    print(x)
     
     if (nrow(x) == 0) {
       return()
@@ -143,50 +201,6 @@ server <- function(input, output, session) {
     
     DT::datatable(df, escape = FALSE)
   })
-  
-  
-  output$lakeSelector <- renderUI({
-    if(length(input$sub_basin)==0) {
-      selectizeInput(inputId = "lakeID",
-                     label = NULL,
-                     choices = c("Please choose a lake ID" = "",lakes$GL_ID),
-                     multiple = TRUE)
-    }
-    else{
-      cur_lakes <- lakes %>%
-        filter(Sub_Basin %in% input$sub_basin) %>%
-        pull(GL_ID)
-      selectizeInput(inputId = "lakeID", 
-                     label = NULL,
-                     choices = c("Please choose a lake ID" = "",cur_lakes),
-                     multiple = TRUE)
-    }
-  })
-  
-  output$basinSelector <- renderUI({
-    if(length(input$lakeID)==0) {
-      selectizeInput(
-        inputId = "sub_basin",
-        label = NULL,
-        choices = c("Please choose a sub basin" = "", lakes$Sub_Basin),
-        multiple = TRUE)
-    }
-    else{
-      cur_basins <- lakes %>%
-        filter(GL_ID %in% input$lakeID) %>%
-        pull(Sub_Basin)
-      print(unique(cur_basins))
-      selectizeInput(
-        inputId = "sub_basin",
-        label = NULL,
-        choices = c("Please choose a sub basin" = "", cur_basins),
-        multiple = TRUE)
-    }
-  })
-  
-  # output$trendSelector <- renderUI({
-  #   
-  # })
   
   
 }
